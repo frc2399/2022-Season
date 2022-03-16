aefcc7691af4a4b64e5276c78f3f5c5e64895e9d
Your Name <rblumin24@hb.edu>
2022-03-16 14:46:20 -0400

    Moved TurnToHub, started the big command to turn, get to position, and shoot

---
+++ src/main/java/frc/robot/commands/robot/PointAndShoot.java
@@ -0,0 +1,51 @@
+package frc.robot.commands.robot;
+
+import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import edu.wpi.first.wpilibj2.command.WaitUntilCommand;
+import frc.robot.Constants.ShooterConstants;
+import frc.robot.commands.TurnToHub;
+import frc.robot.commands.drivetrain.DriveForwardGivenDistance;
+import frc.robot.commands.drivetrain.TurnNAngle;
+import frc.robot.commands.indexer.IndexerCmdForGivenTime;
+import frc.robot.commands.intake.IntakeCmdForGivenTime;
+import frc.robot.commands.intakearm.ExtendIntakeArm;
+import frc.robot.subsystems.*;
+
+// NOTE:  Consider using this command inline, rather than writing a subclass.  For more
+// information, see:
+// https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
+public class PointAndShoot extends SequentialCommandGroup {
+  /** Creates a new IntakeBallShootBothP1. */
+
+  private final DriveTrain m_driveTrain;
+  private final Indexer m_indexer;
+  private final Shooter m_shooter;
+
+  public PointAndShoot(DriveTrain dt, Shooter sh, Indexer id) {
+    m_driveTrain = dt;
+    m_indexer = id;
+    m_shooter = sh;
+
+    // Add your commands in the addCommands() call, e.g.
+    // addCommands(new FooCommand(), new BarCommand());
+
+    addCommands(
+      new TurnToHub(m_driveTrain),
+      new MoveToPosition(m_driveTrain),
+      new ParallelCommandGroup (
+        new TurnNAngle(22.5, m_driveTrain),
+        new InstantCommand(() -> m_shooter.setSpeedWithPID(ShooterConstants.TOP_SETPOINT, ShooterConstants.BOTTOM_SETPOINT), m_shooter) //TODO:fix constatns
+        ),
+      new WaitUntilCommand(() -> m_shooter.correctSpeed()),
+      new IndexerCmdForGivenTime(m_indexer, 0.5, 2)
+
+    );
+  }
+
+  //Turn, then find distance between robot and hub
+  //Get to Goldilocks zone (make command for driving forward to optimal shooting position)
+  //Figure out shooter speed based on distance from hub - function
+  //Shoot! 
+}
