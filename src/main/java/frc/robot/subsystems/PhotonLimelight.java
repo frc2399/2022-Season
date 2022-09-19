bfc835c2319d3bd77873d6b808026ca270bcaf9b
Clare Hess <chess25@hb.edu>
2022-09-19 08:31:39 -0400

    uncommented limelight distance calculations

--- src/main/java/frc/robot/subsystems/PhotonLimelight.java
+++ src/main/java/frc/robot/subsystems/PhotonLimelight.java
@@ -1,5 +1,6 @@
 package frc.robot.subsystems;
 
+import frc.robot.Robot;
 import frc.robot.Constants.PhotonLimelightConstants;
 import frc.robot.util.Coords;
 
@@ -35,22 +36,23 @@ public class PhotonLimelight extends SubsystemBase {
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
-    /*
-     * double y_angle = Robot.ty.getDouble(1);
-     * SmartDashboard.putNumber("y_angle", Robot.ty.getDouble(1));
-     * double y_angle_radians = Math.toRadians(y_angle);
-     * SmartDashboard.putNumber("y_angle_radians", y_angle_radians);
-     * double y_distance =
-     * LimelightConstants.HEIGHT_INCHES/(Math.tan(y_angle_radians));
-     * SmartDashboard.putNumber("y_distance", y_distance);
-     * 
-     * double x_angle = Robot.tx.getDouble(1);
-     * SmartDashboard.putNumber("x_angle", Robot.tx.getDouble(1));
-     * double x_angle_radians = Math.toRadians(x_angle);
-     * SmartDashboard.putNumber("x_angle_radians", x_angle_radians);
-     * double x_distance = y_distance * Math.sin(x_angle_radians);
-     * SmartDashboard.putNumber("x_distance", x_distance);
-     */
+    
+      double y_angle = Robot.ty.getDouble(1);
+      SmartDashboard.putNumber("y_angle", Robot.ty.getDouble(1));
+      double y_angle_radians = Math.toRadians(y_angle);
+      SmartDashboard.putNumber("y_angle_radians", y_angle_radians);
+      double y_distance =
+      (PhotonLimelightConstants.TARGET_HEIGHT_INCHES - PhotonLimelightConstants.CAMERA_HEIGHT_INCHES) 
+         /(Math.tan(y_angle_radians));
+      SmartDashboard.putNumber("y_distance", y_distance);
+      
+      double x_angle = Robot.tx.getDouble(1);
+      SmartDashboard.putNumber("x_angle", Robot.tx.getDouble(1));
+      double x_angle_radians = Math.toRadians(x_angle);
+      SmartDashboard.putNumber("x_angle_radians", x_angle_radians);
+      double x_distance = y_distance * Math.sin(x_angle_radians);
+      SmartDashboard.putNumber("x_distance", x_distance);
+     
     // System.out.println("Testing for photon limelight targets");
 
     PhotonPipelineResult result = null;
